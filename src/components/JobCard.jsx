534fdac81e2f9cabb87b746d3239bc50a2e19aa8
Md Rifat Bin Sumon <mdrifatbinsumon@gmail.com>
2026-03-07 10:48:52 +0600

    feat: implement job details page with dynamic data fetching

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 import { format } from "date-fns"
 
 const JobCard = ({ job }) => {
-  const { title, deadline, description, category, min_price, max_price } = job
+  const { title, deadline, description, category, min_price, max_price, _id } = job
 
   return (
     <Link
-      to={`/job/1`}
+      to={`/job/${_id}`}
       className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all"
     >
       <div className="flex items-center justify-between">
